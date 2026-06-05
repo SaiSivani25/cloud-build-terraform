a4a93bbafdcc76a49aa3f8302a642db5bbfea4a3
SaiSivani25 <dsaisivani@gmail.com>
2026-06-05 16:04:54 -0400

    Add service account impersonation to provider

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project                     = var.project_id
+  region                      = var.region
+  impersonate_service_account = "terraform-deployer@hr-management-2025.iam.gserviceaccount.com"
 }
