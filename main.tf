fd71f14efed38a7b752c70376fc1b5d64885678b
SaiSivani25 <dsaisivani@gmail.com>
2026-06-05 13:49:46 -0400

    Add Docker build, Artifact Registry and Cloud Run

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
+# Existing Storage Bucket
 resource "google_storage_bucket" "my_bucket" {
   name     = var.bucket_name
   location = "US"
+}
+
+# Artifact Registry Repository
+resource "google_artifact_registry_repository" "my_repo" {
+  location      = var.region
+  repository_id = "calculator-repo"
+  format        = "DOCKER"
+}
+
+# Cloud Run Service
+resource "google_cloud_run_v2_service" "calculator" {
+  name     = "calculator-app"
+  location = var.region
+
+  template {
+    containers {
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/calculator-repo/calculator:latest"
+    }
+  }
 }
